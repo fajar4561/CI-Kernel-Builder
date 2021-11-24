af66b4ea43749bb0045737cd682a0e8d25052932
fajar4561 <maulanafajar751@gmail.com>
2021-11-24 21:27:19 +0700

    u

--- build.sh
+++ build.sh
@@ -65,7 +65,7 @@ BUILD_TYPE="Dirty"
 
 # Specify compiler.
 # 'clang' or 'clangxgcc' or 'gcc' or 'gcc49'
-COMPILER=gcc49
+COMPILER=clang
 
 # Message on anykernel when installation
 MESSAGE="don't blame me if u get poor battery backup or weak performance . i'm not responsible . Do with Your Own Risk."
